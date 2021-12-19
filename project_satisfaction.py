ae47803fdea0e03bad73977485e80f44696e447f
ameysonawane26 <93083989+ameysonawane26@users.noreply.github.com>
2021-12-19 17:42:26 +0530

    Update project_satisfaction.py

--- project_satisfaction.py
+++ project_satisfaction.py
@@ -3,21 +3,21 @@ import pandas as pd
 import numpy as np
 from PIL import Image
 
-data = pd.read_excel('Reviews_all_new.xlsx')
+#data = pd.read_excel('Reviews_all_new.xlsx')
 
-X1 = data['Summary'].values.astype('U')
-X2= data['Text']
-y = data['Score']
+#X1 = data['Summary'].values.astype('U')
+#X2= data['Text']
+#y = data['Score']
 
 from sklearn.pipeline import Pipeline
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
 
-clf1 = Pipeline([('vectorizer', CountVectorizer()),('nb', MultinomialNB())])
-clf2 = Pipeline([('vectorizer', CountVectorizer()),('nb', MultinomialNB())])
+#clf1 = Pipeline([('vectorizer', CountVectorizer()),('nb', MultinomialNB())])
+#clf2 = Pipeline([('vectorizer', CountVectorizer()),('nb', MultinomialNB())])
 
-clf1.fit(X1,y)
-clf2.fit(X2,y)
+#clf1.fit(X1,y)
+#clf2.fit(X2,y)
 
 st.title('Customer Satisfaction Prediction')
 
@@ -27,29 +27,30 @@ review = st.text_area('Enter your Food review',key='2')
 
 option = st.selectbox('Please give rating to the product',(1,2,3,4,5),key=4)
 
-num1=clf1.predict([summary])
-num2=clf2.predict([review])
+#num1=clf1.predict([summary])
+#num2=clf2.predict([review])
 
-num3 = (num1+option)/2
+#num3 = (num1+option)/2
 
-if (num3/num2)*100 >=70:
-    number = (num1+num2+option)/3
-else:
-    number = (num1+option)/2
+# if (num3/num2)*100 >=70:
+#     number = (num1+num2+option)/3
+# else:
+#     number = (num1+option)/2
 
 press = st.button('Submit',key=1)
 
 while press:
-    st.header('Prediction:')
-    if number>3:
-        img = Image.open("Screenshot 2021-12-17 183226 (2).jpg")
-        st.image(img)
-        break
-    elif 2<number<4:
-        img1 = Image.open("Screenshot 2021-12-17 175702.jpg")
-        st.image(img1)
-        break
-    else:
-        img2 = Image.open("Screenshot 2021-12-17 183119 (2).jpg")
-        st.image(img2)
-        break
+    st.header(summary,review,option)
+#     st.header('Prediction:')
+#     if number>3:
+#         img = Image.open("Screenshot 2021-12-17 183226 (2).jpg")
+#         st.image(img)
+#         break
+#     elif 2<number<4:
+#         img1 = Image.open("Screenshot 2021-12-17 175702.jpg")
+#         st.image(img1)
+#         break
+#     else:
+#         img2 = Image.open("Screenshot 2021-12-17 183119 (2).jpg")
+#         st.image(img2)
+#         break
