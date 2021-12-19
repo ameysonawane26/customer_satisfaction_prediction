9b255b8da2f88177b807f6e5c0c2c0d90d214928
ameysonawane26 <93083989+ameysonawane26@users.noreply.github.com>
2021-12-19 16:30:13 +0530

    Update project_satisfaction.py

--- project_satisfaction.py
+++ project_satisfaction.py
@@ -3,32 +3,39 @@ import pandas as pd
 import numpy as np
 from PIL import Image
 
-data = pd.read_excel('Reviews_New.xlsx')
-from sklearn.model_selection import train_test_split
+data = pd.read_excel('Reviews_all_new.xlsx')
 
-X_train, X_test, y_train, y_test = train_test_split(data['Text'], data['Score'], test_size=0.20)
+X1 = data['Summary'].values.astype('U')
+X2= data['Text']
+y = data['Score']
 
 from sklearn.pipeline import Pipeline
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
 
-clf = Pipeline([
-    ('vectorizer', CountVectorizer()),
-    ('nb', MultinomialNB())
-])
+clf1 = Pipeline([('vectorizer', CountVectorizer()),('nb', MultinomialNB())])
+clf2 = Pipeline([('vectorizer', CountVectorizer()),('nb', MultinomialNB())])
 
-clf.fit(X_train,y_train)
+clf1.fit(X1,y)
+clf2.fit(X2,y)
+
 st.title('Customer Satisfaction Prediction')
 
+summary = st.text_input('Enter Title of Your review',key='3')
 
-review = st.text_area('Enter your Food review',key=2)
-num=clf.predict([review])
+review = st.text_area('Enter your Food review',key='2')
 
-    
+option = st.selectbox('Please give rating to the product',(1,2,3,4,5),key=4)
 
-option = st.selectbox('Please give rating to the product',(1,2,3,4,5),key=3)
+num1=clf1.predict([summary])
+num2=clf2.predict([review])
 
-number = (num+option)/2
+num3 = (num1+option)/2
+
+if (num3/num2)*100 >=70:
+    number = (num1+num2+option)/3
+else:
+    number = (num1+option)/2
 
 while st.button('Submit',key=1):
     st.header('Prediction:')
@@ -41,4 +48,3 @@ while st.button('Submit',key=1):
     else:
         img2 = Image.open("Screenshot 2021-12-17 183119 (2).jpg")
         st.image(img2)
-
